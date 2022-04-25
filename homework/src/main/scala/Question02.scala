a18391da594e395df59b38722700a57f2b10654e
Plyy <69999838+nutnicha-p@users.noreply.github.com>
2022-04-25 23:08:16 +0700

    ⚡️

--- homework/src/main/scala/Question02.scala
+++ homework/src/main/scala/Question02.scala
@@ -1,3 +1,13 @@
-object Q01 extends App {
-  println("Hello, World!")
+object Q02 extends App {
+    def insertInOrder(
+        x:Int, 
+        list:List[Int]
+    ):List[Int] ={
+        if (list.isEmpty) List(x)
+        else if (x < list.head) x :: list
+        else list.head :: insertInOrder(x, list.tail)
+    }
+    println(insertInOrder(0, List(1,2,4,5)))
+    println(insertInOrder(3, List(1,2,4,5)))
+    println(insertInOrder(6, List(1,2,4,5)))
 }
