b340db4f0f02778e53cfc3dca7024cdf94e5798e
Arm <69001162+arskiir@users.noreply.github.com>
2022-04-25 23:13:26 +0700

    04

--- homework/src/main/scala/Question04.scala
+++ homework/src/main/scala/Question04.scala
@@ -1,3 +1,21 @@
-object Q01 extends App {
-  println("Hello, World!")
+object Q04 extends App {
+  def palindrome(list: List[Any]): Boolean = {
+    def last(list: List[Any]): Any = list match {
+      case Nil      => throw new NoSuchElementException
+      case x :: Nil => x
+      case x :: xs  => last(xs)
+    }
+    def stripHeadAndLast(list: List[Any]): List[Any] = list match {
+      case Nil      => Nil
+      case x :: Nil => Nil
+      case x :: xs  => stripHeadAndLast(xs.tail)
+    }
+
+    if (list.isEmpty || list.length == 1) true
+    else if (list.head == last(list))
+      palindrome(stripHeadAndLast(list))
+    else false
+  }
+
+  println(palindrome(List(1, 2, 1)))
 }
