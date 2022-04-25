9d90063c98d91346811a6da76677c43a39d340d9
Arm <69001162+arskiir@users.noreply.github.com>
2022-04-25 23:00:24 +0700

    07

--- homework/src/main/scala/Question07.scala
+++ homework/src/main/scala/Question07.scala
@@ -1,3 +1,6 @@
 object Q01 extends App {
-  println("Hello, World!")
+  def myMap(f: Int => Int)(list: List[Int]): List[Int] = {
+    if (list.isEmpty) Nil
+    else f(list.head) :: myMap(f)(list.tail)
+  }
 }
